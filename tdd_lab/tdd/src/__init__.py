d30c582eeab230e71344fc26f6f548d5d1ed7613
Alex <villea7@unlv.nevada.edu>
2026-02-15 22:48:50 -0800

    Successful TDD setup and initial test run

---
+++ tdd_lab/tdd/src/__init__.py
@@ -0,0 +1,7 @@
+import pytest
+
+# we need to import the unit under test - counter
+from src.counter import app 
+
+# we need to import the file that contains the status codes
+from src import status 
